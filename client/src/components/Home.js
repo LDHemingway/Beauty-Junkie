8a286e2d9e945f5ac250a413c977c5b6a8fcf27c
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-10-09 10:26:28 -0400

    set up routes for region controller

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,5 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
-    state = {
-        regions: [{
-            name: '',
-            image: '',
-            products: []
-        }]
-    }
-  render() {
-      const regionsList = this.state.regions.map((region, i) => {
-          return (
-              <div>
-                  {regionsList}
-              </div>
-          )
-      })
-    }
-     
+    
+}
--- routes/RegionController.js
+++ routes/RegionController.js
@@ -4,8 +4,23 @@ const { Region } = require('../db/Model')
 
 /* GET home page. */
 router.get('/', async(req, res) => {
-  const region = await Region.find()
+  const regions = await Region.find()
   res.send(regions)
+})
+
+router.get('/:id', async(req, res) => {
+  const region = await Region.findById(req.params.id)
+  res.send(region)
+})
+
+router.post('/', async (req, res) => {
+  const region = await Region.create(req.body)
+  res.send(region)
+})
+
+router.delete('/:id', async(req, res) => {
+  await Region.findByIdAndRemove(req.params.id)
+  res.sendStatus(200)
 })
 
 module.exports = router;
